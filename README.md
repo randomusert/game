5721d26c29e9936524c1eb49fd2022a68521d526
randomusert <eetu.pajamaki@edutornio.net>
2024-11-03 13:43:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# game
+
+a game for github gameoff 2024
+
---
+++ build.sh
@@ -0,0 +1,9 @@
+
+echo building
+#source
+SOURCE="src"
+
+
+npx tailwindcss@3.4.14 -i ./$SOURCE/conf.css -o ./$SOURCE/styles.css --watch
+
+
