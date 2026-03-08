66c33f996d7b59238fea29cca9929b1f527bbc09
randomusert <eetupajamaki@gmail.com>
2026-03-08 15:08:13 +0200

    malware making logic

--- src/javascript/func.js
+++ src/javascript/func.js
@@ -1,5 +1,6 @@
 //variables
 var VirusAmount = 0;
+var MalwareAmount = 0;
 
 
 //functions
@@ -9,3 +10,29 @@ export function MkViruses() {
     Virus.innerHTML = VirusAmount;
 }
 
+//save the game. UNUSED!! Only can be used in the console.
+export function Save() {
+    localStorage.setItem("VirusAmount", VirusAmount);
+}
+//loads the game. UNUSED!! Only can be used in the console.
+export function Load() {
+    let SaveVirusAmount = localStorage.getItem("VirusAmount");
+    if (SaveVirusAmount !== null) {
+        VirusAmount = parseInt(SaveVirusAmount);
+        let Virus = document.getElementById("Virus");
+        Virus.innerHTML = VirusAmount;
+    }
+}
+//resets the game. UNUSED!! Only can be used in the console.
+export function Reset() {
+    localStorage.removeItem("VirusAmount");
+    VirusAmount = 0;
+    let Virus = document.getElementById("Virus");
+    Virus.innerHTML = VirusAmount;
+}
+
+export function MkMalware() {
+    MalwareAmount++;
+    let Malware = document.getElementById("Malware");
+    Malware.innerHTML = MalwareAmount;
+}
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,17 @@
-import { MkViruses } from "./func.js";
+import { MkViruses, Save, Load, Reset , MkMalware} from "./func.js";
+
+var AllowMakingMalware = false;
+
+if (AllowMakingMalware) {
+    document.getElementById("MkMalwareBtn").style.display = "none";
+} else {
+    document.getElementById("MkMalwareBtn").style.display = "block";
+}
 
 if (typeof window !== "undefined") {
     window.MkViruses = MkViruses;
+    window.Save = Save;
+    window.Load = Load;
+    window.Reset = Reset;
+    window.MkMalware = MkMalware;
 }
